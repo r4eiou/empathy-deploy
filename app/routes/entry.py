5396905d591c5b673619e274ff8d405826cdd978
CharlizeBrodeth <ckbrodeth25@gmail.com>
2025-07-16 17:53:25 +0800

    view all entires and entry

--- app/routes/entry.py
+++ app/routes/entry.py
@@ -1,10 +1,21 @@
-from flask import Blueprint, session, redirect, url_for, render_template
+from flask import Blueprint, session, redirect, url_for, render_template, flash
+from app.extensions import mongo 
+from bson import ObjectId
 
 entry_bp = Blueprint("entry", __name__)
 
-@entry_bp.route("/entry", methods=["GET", "POST"])
-def entry():
+@entry_bp.route("/entry/<entry_id>", methods=["GET", "POST"])
+def entry(entry_id):
     if "user_id" not in session:
         return redirect(url_for("auth.index"))
     
-    return render_template("entry.html")
+    entry = mongo.db.journal_entries.find_one({
+        "_id": ObjectId(entry_id),
+        "user_id": session["user_id"]
+    })
+
+    if not entry:
+        flash("Enntry not found.", "error")
+        return redirect(url_for("allEntries.allEntries"))
+
+    return render_template("entry.html", entry=entry)
